cfce70c260f93c59e41d660ce008a0cac8b467a2
beckireby15@gmail.com <beckireby15@gmail.com>
2022-08-18 00:06:37 +0300

    strings variables and interpolation

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Rebecca Steve';
+// const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase() + currentUser + '!';
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
